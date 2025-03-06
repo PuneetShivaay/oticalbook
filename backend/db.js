719375276fa427033f1ca9e41b6af30069caff43
Puneet Kumar <Puneetshivaay@gmail.com>
2025-03-06 17:57:55 +0000

    Mongo DB setup

--- backend/db.js
+++ backend/db.js
@@ -1,2 +1,11 @@
 const mongoose = require('mongoose'); 
-const mongURI = ""
+
+const mongURI = "mongodb+srv://puneetshivaay:iYtn8rukH6Uer7r5@oticalbook.pvpj0.mongodb.net/?retryWrites=true&w=majority&appName=oticalbook"
+
+const connectToMongo = async() =>{
+   await mongoose.connect(mongURI);
+        console.log("Connected to mongo successfully")
+
+}
+
+module.exports = connectToMongo;
